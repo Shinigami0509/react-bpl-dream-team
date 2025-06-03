8353113760f49adc68702e67b52691c54dd28e1a
Shinigami0509 <walidbin.kamal64@gmail.com>
2025-06-03 20:43:19 +0600

    selected player's work is almost done

--- src/components/SelectedPlayer/SelectedPlayer.jsx
+++ src/components/SelectedPlayer/SelectedPlayer.jsx
@@ -1,9 +1,17 @@
-
+import deletebtn from '../../assets/images/delete.png'
 
 const SelectedPlayer = ({selectedPlayer}) => {
+    const {name, player_img, batting_style} = selectedPlayer
     return (
-        <div>
-            <h2>{selectedPlayer.name}</h2>
+        <div className="border border-gray-400 rounded-xl mb-4 flex justify-between items-center">
+            <div className="flex gap-2 items-center">
+                <img className="m-4 w-20 rounded-xl" src={player_img} alt="" />
+                <div>
+                    <h3 className="text-xl font-bold">{name}</h3>
+                    <small>{batting_style}</small>
+                </div>
+            </div>
+            <img className='w-8 h-8 mr-5' src={deletebtn} alt="" />
         </div>
     );
 };
